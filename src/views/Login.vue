dcd83c1c70d2be0c6d4db451bc16450b22a217a5
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-12 14:54:17 -0300

    ♻️ refactor: remove useless comment code

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -31,9 +31,6 @@ export default {
       this.$http
         .post('/auth/login', this.usuario)
         .then(response => {
-          //localStorage.setItem('token', response.data.access_token)
-          // this.store.state.token = response.data.access_token
-          // this.store.state.usuario = response.data.user
           this.$store.commit('DEFINIR_USUARIO_LOGADO', {
             token: response.data.access_token,
             usuario: response.data.user
