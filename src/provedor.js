4d40e945b5b8025d1200e912103269e3e1a6019e
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-12 14:53:20 -0300

    ✨ feat: add mutation to define which user is logged in

--- src/provedor.js
+++ src/provedor.js
@@ -8,6 +8,14 @@ const estado = {
   usuario: {}
 }
 
+const mutations = {
+  DEFINIR_USUARIO_LOGADO(state, { token, usuario }) {
+    state.token = token
+    state.usuario = usuario
+  }
+}
+
 export default new Vuex.Store({
-  state: estado
+  state: estado,
+  mutations
 })
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -31,7 +31,13 @@ export default {
       this.$http
         .post('/auth/login', this.usuario)
         .then(response => {
-          localStorage.setItem('token', response.data.access_token)
+          //localStorage.setItem('token', response.data.access_token)
+          // this.store.state.token = response.data.access_token
+          // this.store.state.usuario = response.data.user
+          this.$store.commit('DEFINIR_USUARIO_LOGADO', {
+            token: response.data.access_token,
+            usuario: response.data.user
+          })
           this.$router.push({ name: 'gerentes' })
         })
         .catch(erro => console.log(erro))
