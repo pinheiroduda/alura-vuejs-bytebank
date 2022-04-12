b435b5d1345d9ee62aad959e789ef3f7ffe9d96a
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-12 11:22:50 -0300

    ✨ feat: creates new user form

--- src/views/NovoUsuario.vue
+++ src/views/NovoUsuario.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Novo Usuário</h1>
+  <div class="container">
+    <h1>Novo Usuário</h1>
+    <form>
+      <div class="form-group">
+        <label for="">Nome</label>
+        <input type="text" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label for="">E-mail</label>
+        <input type="email" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label for="">Senha</label>
+        <input type="password" class="form-control" />
+      </div>
+      <button class="btn btn-primary" type="submit">Salvar</button>
+    </form>
+  </div>
 </template>
