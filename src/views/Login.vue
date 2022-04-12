c90ff51c88dce95c81e7ea4a0024e471cb71a220
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-12 11:52:07 -0300

    ✨ feat: creates Login page

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <form>
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="email" class="form-control" v-model="usuario.email" />
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.senha" />
+      </div>
+      <button type="submit" class="btn btn-primary brn-block">Logar</button>
+      <router-link :to="{ name: 'novo.usuario' }">
+        Não possui um cadastro? Cadastre-se aqui
+      </router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: {
+        email: '',
+        senha: ''
+      }
+    }
+  }
+}
+</script>
