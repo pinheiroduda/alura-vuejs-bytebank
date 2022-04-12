ec333c1ac688a819949f4e28eba3c5ba61cc46ee
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-12 11:28:51 -0300

    ✨ feat: adds data form submission

--- src/views/NovoUsuario.vue
+++ src/views/NovoUsuario.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="container">
     <h1>Novo Usuário</h1>
-    <form>
+    <form @submit.prevent="enviarFormulario">
       <div class="form-group">
         <label for="">Nome</label>
-        <input type="text" class="form-control" />
+        <input type="text" class="form-control" v-model="usuario.nome" />
       </div>
       <div class="form-group">
         <label for="">E-mail</label>
-        <input type="email" class="form-control" />
+        <input type="email" class="form-control" v-model="usuario.email" />
       </div>
       <div class="form-group">
         <label for="">Senha</label>
-        <input type="password" class="form-control" />
+        <input type="password" class="form-control" v-model="usuario.senha" />
       </div>
       <button class="btn btn-primary" type="submit">Salvar</button>
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: {
+        nome: '',
+        email: '',
+        senha: ''
+      }
+    }
+  },
+
+  methods: {
+    enviarFormulario() {
+      console.log(this.usuario)
+    }
+  }
+}
+</script>
