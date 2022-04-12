880db20076112306960363300148b275f02336e0
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-12 11:52:58 -0300

    ✨ feat: adds name to for in new user form fields

--- src/views/NovoUsuario.vue
+++ src/views/NovoUsuario.vue
@@ -3,15 +3,15 @@
     <h1>Novo Usuário</h1>
     <form @submit.prevent="enviarFormulario">
       <div class="form-group">
-        <label for="">Nome</label>
+        <label for="nome">Nome</label>
         <input type="text" class="form-control" v-model="usuario.nome" />
       </div>
       <div class="form-group">
-        <label for="">E-mail</label>
+        <label for="email">E-mail</label>
         <input type="email" class="form-control" v-model="usuario.email" />
       </div>
       <div class="form-group">
-        <label for="">Senha</label>
+        <label for="senha">Senha</label>
         <input type="password" class="form-control" v-model="usuario.senha" />
       </div>
       <button class="btn btn-primary" type="submit">Salvar</button>
