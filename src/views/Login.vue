911907e48530e932c0f118be02cb3a4b57f985bb
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-13 09:47:50 -0300

    ✨ feat: adds empty error message when login is made with success

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,6 +34,7 @@ export default {
         .dispatch('efetuarLogin', this.usuario)
         .then(() => {
           this.$router.push({ name: 'gerentes' })
+          this.mensagemErro = ''
         })
         .catch(err => {
           if (err.request.status === 401) {
