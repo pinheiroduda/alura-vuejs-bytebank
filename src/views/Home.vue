0b337f73903a8d8057398012229cc855a5096a25
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-13 09:27:36 -0300

    ✨ feat: adds logout option in home route component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,20 @@
   <div class="container">
     <div class="jumbotron mt-2">
       <h1 class="display-4">Bem-vindo!</h1>
-      <p>Aplição disponível para listagem dos gerentes do nosso banco.</p>
+      <button class="btn btn-danger" @click="efetuarLogout">Sair</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+
+  methods: {
+    efetuarLogout() {
+      this.$store.commit('DESLOGAR_USUARIO')
+      this.$router.push({ name: 'login' })
+    }
+  }
 }
 </script>
