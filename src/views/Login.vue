adcaa666905aaff547234e7eabaf430afefc3c8d
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-13 09:45:29 -0300

    ✨ feat: displays error message when user login or password are incorrect

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,6 +10,7 @@
         <label for="senha">Senha</label>
         <input type="password" class="form-control" v-model="usuario.senha" />
       </div>
+      <p class="alert alert-danger" v-if="mensagemErro">{{ mensagemErro }}</p>
       <button type="submit" class="btn btn-primary brn-block">Logar</button>
       <router-link :to="{ name: 'novo.usuario' }">
         Não possui um cadastro? Cadastre-se aqui
@@ -22,15 +23,23 @@
 export default {
   data() {
     return {
-      usuario: {}
+      usuario: {},
+      mensagemErro: ''
     }
   },
 
   methods: {
     efetuarLogin() {
-      this.$store.dispatch('efetuarLogin', this.usuario).then(() => {
-        this.$router.push({ name: 'gerentes' })
-      })
+      this.$store
+        .dispatch('efetuarLogin', this.usuario)
+        .then(() => {
+          this.$router.push({ name: 'gerentes' })
+        })
+        .catch(err => {
+          if (err.request.status === 401) {
+            this.mensagemErro = 'Login ou senha inválido(s)'
+          }
+        })
     }
   }
 }
